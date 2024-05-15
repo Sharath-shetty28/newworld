1b06c99476dd5190202cbcd7f6ea7fd737d771d5
SHARATH SHETTY <130232996+Sharath-shetty28@users.noreply.github.com>
2024-05-15 20:14:26 +0530

    Update b3.php

--- b3.php
+++ b3.php
@@ -1,41 +1,61 @@
-<!DOCTYPE html>
-<html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Distance Calculator</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Distance</title>
 </head>
+
 <body>
-    <h2>Enter two distances in feet and inches</h2>
-    <form action="" method="post">
-        Distance 1:
-        <input type="text" name="feet1" placeholder="Feet"> feet
-        <input type="text" name="inches1" placeholder="Inches"> inches
-        <br><br>
-        Distance 2:
-        <input type="text" name="feet2" placeholder="Feet"> feet
-        <input type="text" name="inches2" placeholder="Inches"> inches
-        <br><br>
-        <input type="submit" name="submit" value="Calculate">
-    </form>
+<h2>Distance</h2>
+<form action="" method="post">
+Distance1:<br />
+Feet1:<input type="number" name="feet1" required="required" /><br />
+Inches1:<input type="number" name="inches1" required /><br />
+Distance2:<br />
+Feet2:<input type="number" name="feet2" required="required" /><br />
+Inches2:<input type="number" name="inches2" required /><br />
+<br />
+<input type="submit" name="calculate" value="calculate" />
+</form>
+<?php
+class DistanceCalculator
+{
+	public static function add($feet1,$inches1,$feet2,$inches2)
+	{
+		$total_feet = $feet1 + $feet2;
+		$total_inches = $inches1 + $inches2;
+		if($total_inches>=12)
+		{
+			$total_feet += floor($total_inches/12);
+			$total_inches %=12;
+		}
+		return array($total_feet,$total_inches);
+	}
+	public static function subtract($feet1,$inches1,$feet2,$inches2)
+	{
+		$total_feet = $feet1 - $feet2;
+		$total_inches = $inches1 - $inches2;
+		if($total_inches<0)
+		{
+			$total_feet -=1;
+			$total_inches +=12;
+		}
+		return array($total_feet,$total_inches);
+	}
+}
+if(isset($_POST["calculate"]))
+{
+	$feet1 = $_POST["feet1"];
+	$inches1 = $_POST["inches1"];
+	$feet2 = $_POST["feet2"];
+	$inches2 = $_POST["inches2"];
+	list($sum_feet,$sum_inches) = DistanceCalculator::add($feet1,$inches1,$feet2,$inches2);
+	list($diff_feet,$diff_inches) = DistanceCalculator::subtract($feet1,$inches1,$feet2,$inches2);
+	echo"<br>Sum:$sum_feet feet $sum_inches inches <br>";
+	echo"<br>Diff:$diff_feet feet $diff_inches inches";
+}
 
-    <?php
-    if (isset($_POST['submit'])) {
-        $feet1 = $_POST['feet1'];
-        $inches1 = $_POST['inches1'];
-        $feet2 = $_POST['feet2'];
-        $inches2 = $_POST['inches2'];
-
-        // Calculate total inches for each distance
-        $totalInches1 = $feet1 * 12 + $inches1;
-        $totalInches2 = $feet2 * 12 + $inches2;
-
-        // Calculate sum and difference
-        $sum = $totalInches1 + $totalInches2;
-        $difference = abs($totalInches1 - $totalInches2);
-
-        echo "<h2>Results</h2>";
-        echo "Sum: " . floor($sum / 12) . " feet " . ($sum % 12) . " inches<br>";
-        echo "Difference: " . floor($difference / 12) . " feet " . ($difference % 12) . " inches";
-    }
-    ?>
+	
+?>
 </body>
 </html>
